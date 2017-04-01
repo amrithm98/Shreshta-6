804c3d5756857f7a14f8c23480fbf8648387a1cf
amrithm98 <amrithchallenger@gmail.com>
2017-04-01 17:09:06 +0530

    Service

--- app/src/main/java/shreshta/com/air_help/MainActivity.java
+++ app/src/main/java/shreshta/com/air_help/MainActivity.java
@@ -4,16 +4,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-import shreshta.com.air_help.ServiceHelpers.NotificationEventReceiver;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-    public void onSendNotificationsButtonClick(View view) {
-        NotificationEventReceiver.setupAlarm(getApplicationContext());
     }
 }
